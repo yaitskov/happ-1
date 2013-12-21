0bf1156995d4e30ba754f01ae58a188e57415b67
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-12-22 02:02:44 +0400

    access to the argument

--- main.hs
+++ main.hs
@@ -1,20 +1,21 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
-
-
--- import Happstack.Server (nullConf, simpleHTTP, toResponse, ok)
-
--- main :: IO ()
--- main = do simpleHTTP nullConf $ ok "Hello, World!"
 
 import Happstack.Server
 import Control.Monad
 
--- main :: IO ()
 main = simpleHTTP nullConf $ msum pathes
 
 pathes = [ dir "download" $ ok "git clone from http://github.com/yaitskov",
            dir "info" $ ok "mini wiki",
+           dir "path" $ path helloPage,
            ok "home page"
          ]
+
+len :: String -> Int
+len s = length s
+
+helloPage s = do
+   ok $ "hello " ++ (show (len s)) -- (show $ length (
 
 
